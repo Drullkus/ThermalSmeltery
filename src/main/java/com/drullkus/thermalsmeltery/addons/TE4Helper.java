39b6fd06cfe4055354ec39daf6813cde4080a4d8
Drullkus <ericsplanet@mac.com>
2015-02-11 16:32:03 -0800

    Adding Pulverizer Recipes for no reason whatsoever

--- src/main/java/com/drullkus/thermalsmeltery/addons/TE4Helper.java
+++ src/main/java/com/drullkus/thermalsmeltery/addons/TE4Helper.java
@@ -7,6 +7,40 @@ import cpw.mods.fml.common.event.FMLInterModComms;
 
 public class TE4Helper {
 
+    public static void addPulverizerRecipe (int energy, ItemStack input, ItemStack primaryOutput)
+    {
+
+        addPulverizerRecipe(energy, input, primaryOutput, null, 0);
+    }
+
+    public static void addPulverizerRecipe (int energy, ItemStack input, ItemStack primaryOutput, ItemStack secondaryOutput)
+    {
+
+        addPulverizerRecipe(energy, input, primaryOutput, secondaryOutput, 100);
+    }
+
+    public static void addPulverizerRecipe (int energy, ItemStack input, ItemStack primaryOutput, ItemStack secondaryOutput, int secondaryChance)
+    {
+
+        if (input == null || primaryOutput == null || secondaryOutput == null)
+        {
+            return;
+        }
+        NBTTagCompound toSend = new NBTTagCompound();
+
+        toSend.setInteger("energy", energy);
+        toSend.setTag("input", new NBTTagCompound());
+        toSend.setTag("primaryOutput", new NBTTagCompound());
+        toSend.setTag("secondaryOutput", new NBTTagCompound());
+
+        input.writeToNBT(toSend.getCompoundTag("input"));
+        primaryOutput.writeToNBT(toSend.getCompoundTag("primaryOutput"));
+        secondaryOutput.writeToNBT(toSend.getCompoundTag("secondaryOutput"));
+        toSend.setInteger("secondaryChance", secondaryChance);
+
+        FMLInterModComms.sendMessage("ThermalExpansion", "PulverizerRecipe", toSend);
+    }
+
     public static void addCrucibleRecipe (int energy, ItemStack input, FluidStack output)
     {
 
@@ -25,7 +59,5 @@ public class TE4Helper {
         output.writeToNBT(toSend.getCompoundTag("output"));
 
         FMLInterModComms.sendMessage("ThermalExpansion", "CrucibleRecipe", toSend);
-
     }
-
 }
