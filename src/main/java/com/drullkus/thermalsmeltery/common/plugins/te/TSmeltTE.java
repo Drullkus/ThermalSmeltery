37b6c3119d179b59c483d98d777f887aa4d9a5ff
Drullkus <ericsplanet@mac.com>
2015-02-17 19:23:20 -0800

    Add Null Voiding Modifier Recipe; Bump version

--- src/main/java/com/drullkus/thermalsmeltery/common/plugins/te/TSmeltTE.java
+++ src/main/java/com/drullkus/thermalsmeltery/common/plugins/te/TSmeltTE.java
@@ -2,6 +2,8 @@ package com.drullkus.thermalsmeltery.common.plugins.te;
 
 import java.util.Map;
 
+import com.drullkus.thermalsmeltery.common.items.ModItems;
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 import mantle.pulsar.pulse.Handler;
@@ -20,18 +22,16 @@ public class TSmeltTE
 {
 
     @Handler
-    public void preInit (FMLPostInitializationEvent event)
-    {
+    public void preInit (FMLPostInitializationEvent event) {
         Map<ItemMetaWrapper, FluidStack> smelteryMap = tconstruct.library.crafting.Smeltery.getSmeltingList();
         Map<ItemMetaWrapper, Integer> tempMap = tconstruct.library.crafting.Smeltery.getTemperatureList();
 
-        for (Map.Entry<ItemMetaWrapper, FluidStack> entry : smelteryMap.entrySet())
-        {
+        for (Map.Entry<ItemMetaWrapper, FluidStack> entry : smelteryMap.entrySet()) {
             ItemStack input = new ItemStack(entry.getKey().item, 1, entry.getKey().meta);
             int energy = tempMap.get(entry.getKey()) * 10 * TSmeltConfig.multiplier;
             TE4Helper.addCrucibleRecipe(energy, input, entry.getValue());
         }
 
+        TE4Helper.addPulverizerRecipe(20000, new ItemStack(GameRegistry.findBlock("ThermalExpansion", "Device"), 1, 5), new ItemStack(ModItems.Tool_Mod_Void, 1, 0), null, 0);
     }
-
 }
