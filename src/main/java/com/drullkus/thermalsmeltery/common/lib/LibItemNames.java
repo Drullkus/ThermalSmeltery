0a83df87a5227ae35ff1f6508231786abc45d45b
Drullkus <ericsplanet@mac.com>
2015-02-21 15:51:01 -0800

    Fixing weird texture screw-up that works on dev but not obfuscated

--- src/main/java/com/drullkus/thermalsmeltery/common/lib/LibItemNames.java
+++ src/main/java/com/drullkus/thermalsmeltery/common/lib/LibItemNames.java
@@ -7,7 +7,7 @@ public class LibItemNames {
 	// Mod
     public static final String POTATOES_MASHED = LibResources.PREFIX_MOD + "potatoesMashed";
     public static final String POTATOES_MASHED_NAME = "potatoesMashed";
-    public static final String TOOLMOD_VOID = LibResources.PREFIX_MOD + "upgradeparts.PerditioVoid";
-    public static final String TOOLMOD_VOID_NAME = "upgradeparts.PerditioVoid";
+    public static final String TOOLMOD_VOID = LibResources.PREFIX_MOD + "upgradeparts.Voiding";
+    public static final String TOOLMOD_VOID_NAME = "upgradeparts.Voiding";
 	
 }
