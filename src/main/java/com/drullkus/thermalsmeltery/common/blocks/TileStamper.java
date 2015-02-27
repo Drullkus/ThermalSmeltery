6cf2089ae30396c63292034032dfc251dd202c55
hilburn <charliepaterson89@gmail.com>
2015-02-27 13:43:14 +0000

    update

--- src/main/java/com/drullkus/thermalsmeltery/common/blocks/TileStamper.java
+++ src/main/java/com/drullkus/thermalsmeltery/common/blocks/TileStamper.java
@@ -15,12 +15,12 @@ public class TileStamper extends TileSmelteryBase
 
     public static void initialize() {
         defaultSideConfigSmeltery[TYPE] = new SideConfig();
-        defaultSideConfigSmeltery[TYPE].numGroup = 3;
-        defaultSideConfigSmeltery[TYPE].slotGroups = new int[][]{new int[0], {0, 1}, new int[0]};
-        defaultSideConfigSmeltery[TYPE].allowInsertion = new boolean[]{false, false, false};
-        defaultSideConfigSmeltery[TYPE].allowExtraction = new boolean[]{false, true, false};
-        defaultSideConfigSmeltery[TYPE].sideTex = new int[]{0, 1, 4};
-        defaultSideConfigSmeltery[TYPE].defaultSides = new byte[]{(byte)1, (byte)1, (byte)2, (byte)2, (byte)2, (byte)2};
+        defaultSideConfigSmeltery[TYPE].numGroup = 5;
+        defaultSideConfigSmeltery[TYPE].slotGroups = new int[][]{new int[0], {0}, {1}, new int[0], {1}};
+        defaultSideConfigSmeltery[TYPE].allowInsertion = new boolean[]{false, true, false, false, false};
+        defaultSideConfigSmeltery[TYPE].allowExtraction = new boolean[]{false, true, true, false, true};
+        defaultSideConfigSmeltery[TYPE].sideTex = new int[]{0, 1, 2, 3, 4};
+        defaultSideConfigSmeltery[TYPE].defaultSides = new byte[]{(byte)3, (byte)1, (byte)2, (byte)2, (byte)2, (byte)2};
 //        int basePower = MathHelper.clampI(ThermalExpansion.config.get("block.tweak", "Machine.Crucible.BasePower", 400), 100, 500);
 //        ThermalExpansion.config.set("block.tweak", "Machine.Crucible.BasePower", var0);
         int basePower = 400;
@@ -37,7 +37,7 @@ public class TileStamper extends TileSmelteryBase
 
     public TileStamper()
     {
-        this.inventory = new ItemStack[4];
+        this.inventory = new ItemStack[3];
     }
 
     protected boolean hasValidInput() {
