23c68afb4372b786db11712c15b518cc606d5cdb
Drullkus <ericsplanet@mac.com>
2016-01-04 20:06:15 -0800

    More work on RF Smeltery - Sizing Algorithm

--- src/main/java/us/drullk/thermalsmeltery/common/tile/TileRFSmelteryLogic.java
+++ src/main/java/us/drullk/thermalsmeltery/common/tile/TileRFSmelteryLogic.java
@@ -7,7 +7,6 @@ import mantle.blocks.iface.IActiveLogic;
 import mantle.blocks.iface.IFacingLogic;
 import mantle.blocks.iface.IMasterLogic;
 import mantle.blocks.iface.IServantLogic;
-import mantle.world.CoordTuple;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.init.Blocks;
@@ -17,29 +16,22 @@ import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.*;
 import tconstruct.TConstruct;
+import us.drullk.thermalsmeltery.ThermalSmeltery;
 import us.drullk.thermalsmeltery.common.core.handler.TSmeltConfig;
 
 public class TileRFSmelteryLogic extends InventoryLogic implements IActiveLogic, IFacingLogic, IFluidTank, IMasterLogic, IEnergyReceiver, IEnergyStorage, IFluidHandler
 {
+	private boolean debug = true;
+
 	private int tick;
 
 	private final int maxTick = TSmeltConfig.tConSmelteryTickFrequency >= 120 ? 120 : TSmeltConfig.tConSmelteryTickFrequency;
 
-	public static final int MAX_SMELTERY_SIZE_RADIUS = 7, MB_P_BLOCK = TConstruct.ingotLiquidValue * 20;
+	public static final int MAX_SMELTERY_SIZE_DIAMETER = 8, MB_P_BLOCK = TConstruct.ingotLiquidValue * 20;
 
 	public boolean validStructure;
 
-	public boolean tempValidStructure;
-
 	protected byte direction;
-
-	public CoordTuple minPos = new CoordTuple(0, 0, 0);
-
-	public CoordTuple maxPos = new CoordTuple(0, 0, 0);
-
-	public int layers;
-
-	public int maxBlockCapacity;
 
 	public TileRFSmelteryLogic(int invSize)
 	{
@@ -68,8 +60,22 @@ public class TileRFSmelteryLogic extends InventoryLogic implements IActiveLogic,
 			tick = 0;
 			//detectEntities();
 		}
+		int radius = getSmelteryDiameter(xCoord, yCoord, zCoord);
 
-		checkValidPlacement(); //temp
+		if(radius != -1)
+		{
+			if(debug)
+			{
+				ThermalSmeltery.logger.info("Radius: " + radius);
+			}
+		}
+		else
+		{
+			if(debug)
+			{
+				ThermalSmeltery.logger.warn("Invalid RF Smeltery Construction!");
+			}
+		}
 	}
 
 	@Override
@@ -150,80 +156,183 @@ public class TileRFSmelteryLogic extends InventoryLogic implements IActiveLogic,
 		checkValidPlacement();
 	}
 
-	public void checkValidPlacement()
+	public int[] checkValidPlacement()
 	{
+		final int[] initialPlacement;
+
 		switch(getRenderDirection())
 		{
 			case 2: // +z
-				alignInitialPlacement(xCoord, yCoord, zCoord + 1);
+				initialPlacement = new int[]{xCoord, yCoord, zCoord + 1};
 				break;
 			case 3: // -z
-				alignInitialPlacement(xCoord, yCoord, zCoord - 1);
+				initialPlacement = new int[]{xCoord, yCoord, zCoord - 1};
 				break;
 			case 4: // +x
-				alignInitialPlacement(xCoord + 1, yCoord, zCoord);
+				initialPlacement = new int[]{xCoord + 1, yCoord, zCoord};
 				break;
 			case 5: // -x
-				alignInitialPlacement(xCoord - 1, yCoord, zCoord);
+				initialPlacement = new int[]{xCoord - 1, yCoord, zCoord};
+				break;
+			default:
+				initialPlacement = new int[]{xCoord, yCoord, zCoord};
+				if(debug)
+				{
+					ThermalSmeltery.logger.warn("There was a problem with determining rotation direction of RFSmeltery!");
+				}
 				break;
 		}
+
+		return initialPlacement;
 	}
 
-	public void alignInitialPlacement(int x, int y, int z)
+	public int getSmelteryDiameter(int x, int y, int z)
 	{
 		// Parameters x, y, z should "put" us in the smeltery. Or rather, scanning the empty space.
 		// checkValidPlacement() puts this scanning method "inside the smeltery"
+		// Returns the diameter
+		int diameterSpaceVerified = 0;
+
+		switch(getRenderDirection())
+		{
+			case 2: // +z
+				for(int c = 0; c < MAX_SMELTERY_SIZE_DIAMETER || c == diameterSpaceVerified; c++)
+				{
+					if(worldObj.getBlock(x, y, z + c) == null || worldObj.isAirBlock(x, y, z + c))
+					{
+						diameterSpaceVerified++;
+					}
+				}
+				break;
+			case 3: // -z
+				for(int c = 0; c < MAX_SMELTERY_SIZE_DIAMETER || c == diameterSpaceVerified; c++)
+				{
+					if(worldObj.getBlock(x, y, z - c) == null || worldObj.isAirBlock(x, y, z - c))
+					{
+						diameterSpaceVerified++;
+					}
+				}
+				break;
+			case 4: // +x
+				for(int c = 0; c < MAX_SMELTERY_SIZE_DIAMETER || c == diameterSpaceVerified; c++)
+				{
+					if(worldObj.getBlock(x + c, y, z) == null || worldObj.isAirBlock(x + c, y, z))
+					{
+						diameterSpaceVerified++;
+					}
+				}
+				break;
+			case 5: // -x
+				for(int c = 0; c < MAX_SMELTERY_SIZE_DIAMETER || c == diameterSpaceVerified; c++)
+				{
+					if(worldObj.getBlock(x - c, y, z) == null || worldObj.isAirBlock(x - c, y, z))
+					{
+						diameterSpaceVerified++;
+					}
+				}
+				break;
+			default:
+				if(debug)
+				{
+					ThermalSmeltery.logger.warn("There was a problem with determining rotation direction of RFSmeltery!");
+				}
+				return -1;
+		}
+
+		return diameterSpaceVerified;
+	}
+
+	/*public int getSmelteryDiameter(int x, int y, int z)
+	{
+		// Parameters x, y, z should "put" us in the smeltery. Or rather, scanning the empty space.
+		// checkValidPlacement() puts this scanning method "inside the smeltery"
+		// Returns the radius, excluding the center block.
 
 		// Adjust the x-position of the block until the difference between the outer walls is at most 1
 		// basically this means we center the block inside the smeltery on the x axis.
 		// Smeltery must be square!
 		int xDiff1 = 1, xDiff2 = 1; // x-difference
 
-		for (int i = 1; i < MAX_SMELTERY_SIZE_RADIUS; i++) // Don't check farther than needed (MAX_SMELTERY_SIZE_RADIUS)
+		for(int i = 1; i < MAX_SMELTERY_SIZE_DIAMETER; i++) // Don't check farther than needed (MAX_SMELTERY_SIZE_DIAMETER)
 		{
-			if (xDiff1 == i && (worldObj.getBlock(x - xDiff1, y, z) == null || worldObj.isAirBlock(x - xDiff1, y, z)))
+			if(xDiff1 == i && (worldObj.getBlock(x - xDiff1, y, z) == null || worldObj.isAirBlock(x - xDiff1, y, z)))
 			{
-				worldObj.setBlock(x - xDiff1, y, z, Blocks.diamond_ore);
+				if(debug)
+				{
+					worldObj.setBlock(x - xDiff1, y + 1, z, Blocks.redstone_ore);
+				}
 				xDiff1++;
 			}
 
-			else if (xDiff2 == i && (worldObj.getBlock(x + xDiff2, y, z) == null || worldObj.isAirBlock(x + xDiff2, y, z)))
+			if(xDiff2 == i && (worldObj.getBlock(x + xDiff2, y, z) == null || worldObj.isAirBlock(x + xDiff2, y, z)))
 			{
-				worldObj.setBlock(x + xDiff2,y,z, Blocks.diamond_ore);
+				if(debug)
+				{
+					worldObj.setBlock(x + xDiff2, y + 1, z, Blocks.diamond_ore);
+				}
 				xDiff2++;
 			}
 		}
 
 		// Same for z-axis
 		int zDiff1 = 1, zDiff2 = 1;
-		for (int i = 1; i < MAX_SMELTERY_SIZE_RADIUS; i++) // Don't check farther than needed
+		for(int i = 1; i < MAX_SMELTERY_SIZE_DIAMETER; i++) // Don't check farther than needed
 		{
 			if(zDiff1 == i && (worldObj.getBlock(x, y, z - zDiff1) == null || worldObj.isAirBlock(x, y, z - zDiff1)))
 			{
-				worldObj.setBlock(x,y,z - zDiff1, Blocks.emerald_ore);
+				if(debug)
+				{
+					worldObj.setBlock(x, y + 1, z - zDiff1, Blocks.emerald_ore);
+				}
 				zDiff1++;
 			}
-			else if(zDiff2 == i && (worldObj.getBlock(x, y, z + zDiff2) == null || worldObj.isAirBlock(x, y, z + zDiff2)))
+
+			if(zDiff2 == i && (worldObj.getBlock(x, y, z + zDiff2) == null || worldObj.isAirBlock(x, y, z + zDiff2)))
 			{
-				worldObj.setBlock(x,y,z - zDiff2, Blocks.emerald_ore);
+				if(debug)
+				{
+					worldObj.setBlock(x, y + 1, z + zDiff2, Blocks.gold_ore);
+				}
 				zDiff2++;
 			}
 		}
 
-		int[] sides = new int[] { xDiff1, xDiff2, zDiff1, zDiff2 };
-		checkValidStructure(x, y, z, sides);
-	}
+		if((xDiff1 == xDiff2))
+		{
+			if(xDiff1 + xDiff2 - 1 == (zDiff1 > zDiff2 ? zDiff1 : zDiff2))
+			{
+				//System.out.println(zDiff1 + " and " + zDiff2 + " at " + xCoord + ", "+ yCoord + ", "+ zCoord);
+				return xDiff1;
+			}
+		}
+
+		if((zDiff1 == zDiff2))
+		{
+			if(zDiff1 + zDiff2 - 1 == (xDiff1 > xDiff2 ? xDiff1 : xDiff2))
+			{
+				//System.out.println(xDiff1 + " and " + xDiff2 + " at " + xCoord + ", "+ yCoord + ", "+ zCoord);
+				return zDiff1;
+			}
+		}
+
+		if(debug)
+		{
+			ThermalSmeltery.logger.warn("There was a problem! " + xDiff1 + ", " + xDiff2 + ", " + zDiff1 + ", " + zDiff2);
+		}
+		return -1;
+	}*/
 
 	public void checkValidStructure(int x, int y, int z, int[] sides)
 	{
 		int xSize, ySize, zSize;
 
-		xSize = xCoord;
-		zSize = zCoord;
+		if(debug)
+		{
+			worldObj.setBlock(x, y + 1, z, Blocks.redstone_block);
+			worldObj.setBlock(x - sides[0] + 1, y + 2, z - sides[2] + 1, Blocks.diamond_block);
+			worldObj.setBlock(x + sides[1] - 1, y + 2, z + sides[3] - 1, Blocks.emerald_block);
+		}
 
-		worldObj.setBlock(x,y,z, Blocks.redstone_block);
-		worldObj.setBlock(x-sides[0], y+1, z-sides[2], Blocks.diamond_block);
-		worldObj.setBlock(x+sides[1], y+1, z+sides[3], Blocks.emerald_block);
 	}
 
 	@Override
